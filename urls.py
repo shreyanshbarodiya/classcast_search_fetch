7ed4fa8c089710b8c1ae7218cdf6ff43a061569d
shreyansh <shreyansh.barodiya@gmail.com>
2018-05-29 13:55:07 +0530

    pushing to remote gcp

--- urls.py
+++ urls.py
@@ -5,7 +5,7 @@ import views
 
 urlpatterns = [
     url(r'^search/$', views.QuestionSearchAPIView.as_view(), name='search'),
-    # url(r'^test/$', views.QuestionTestAPIView.as_view(), name='test'),
+    url(r'^test/$', views.QuestionTestAPIView.as_view(), name='test'),
     # url(r'^challenge/$', views.QuestionChallengeAPIView.as_view(), name='challenge'),
     # url(r'^gym/$', views.QuestionGymAPIView.as_view(), name='gym'),
 		]   
